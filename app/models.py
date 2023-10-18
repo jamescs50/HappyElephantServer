e6cacd10c85e3cf6b4ec48470fc548014b5b919a
James Carr-Saunders <jamescs@btinterent.com>
2023-10-18 20:55:27 +0100

    added data view

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 
 class WeatherData(db.Model):
+    __tablename__ = 'mqttlog'
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     source_topic = db.Column(db.String(120), index=True)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from app import app
+from app.models import  WeatherData
 import io
-from flask import Response
+from flask import Response,render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Welcome to the cult of the happy elephant."
+    return render_template('app/dashboard.html', title=('Home'))
+
+@app.route('/data')
+def data():
+    d = WeatherData.query.all()
+    return  render_template('app/data.html',title='Data',data=d)
